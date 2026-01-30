a8d3c2f4f21ad96800acfcf98a65d1c231d40c90
Kinoli7 <kinoli@hotmail.com>
2026-01-30 23:02:15 +0100

    Main Scene created with input + bar

---
+++ Assets/Scripts/Picar.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Picar : MonoBehaviour
+{
+    public Slider picarSlider;
+    public InputActionReference picarAction;
+
+    void OnEnable()
+    {
+        picarAction.action.performed += OnProgress;
+        picarAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        picarAction.action.performed -= OnProgress;
+        picarAction.action.Disable();
+    }
+
+    void OnProgress(InputAction.CallbackContext ctx)
+    {
+        picarSlider.value += 0.1f;
+        //progressBar.value += step;
+        //progressBar.value = Mathf.Clamp01(progressBar.value);
+
+        //if (progressBar.value >= 1f)
+        //{
+        //    Debug.Log("¡Barra completa!");
+        //}
+        //Debug.Log("1");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
