415aa18950be11300313b5cce4c33e4e85687396
Kinoli7 <kinoli@hotmail.com>
2026-02-01 12:33:21 +0100

    Added story screen after selecting new game

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,10 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    public void goToGame()
+    public GameObject BlackBackgroundAndText;
+    public Text PressAnyKeyText;
+    public float TextAlphaHighlightStep = 0.1f;
+    float TextAlpha = 0.0f;
+    public InputActionReference anyKeyInputAction;
+
+    private void Start()
     {
+        BlackBackgroundAndText.SetActive(false);
+        PressAnyKeyText.color = new Color(1.0f, 1.0f, 1.0f, TextAlpha);
+    }
+
+    void Update()
+    {
+        TextAlpha += TextAlphaHighlightStep;
+        PressAnyKeyText.color = new Color(1.0f, 1.0f, 1.0f, TextAlpha);
+
+        if (TextAlpha <= 0.0f || TextAlpha >= 1.0f)
+        {
+            TextAlphaHighlightStep *= -1;
+        }
+    }
+
+    void OnPressedKey(InputAction.CallbackContext ctx)
+    {
+        anyKeyInputAction.action.performed -= OnPressedKey;
+        anyKeyInputAction.action.Disable();
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void showIntro()
+    {
+        BlackBackgroundAndText.SetActive(true);
+        anyKeyInputAction.action.performed += OnPressedKey;
+        anyKeyInputAction.action.Enable();
     }
 }
